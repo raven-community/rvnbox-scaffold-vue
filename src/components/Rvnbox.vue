767ed0df0f784e16a3dd7a5369aec6fa8041352f
MSFTserver <sawyerlemmo@gmail.com>
2018-12-22 11:36:46 -0800

    update bip44

--- src/components/Rvnbox.vue
+++ src/components/Rvnbox.vue
@@ -10,12 +10,12 @@
       <h3>BIP44 Account</h3>
       <p>
         <code>
-        "m/44'/145'/0'"
+        "m/0'/175'/0'"
         </code>
       </p>
       <h3>BIP44 external change addresses</h3>
       <ul>
-        <li v-for="(address, index) in addresses" :key="index">m/44&rsquo;/145&rsquo;/0&rsquo;/0/{{index}}:{{address}}</li>
+        <li v-for="(address, index) in addresses" :key="index">m/40&rsquo;/175&rsquo;/0&rsquo;/0/{{index}}:{{address}}</li>
       </ul>
       <h3>Transaction raw hex</h3>
       <p>{{hex}}</p>
@@ -50,18 +50,18 @@ let rootSeed = RVNBOX.Mnemonic.toSeed(mnemonic)
 let masterHDNode = RVNBOX.HDNode.fromSeed(rootSeed, 'ravencoin')
 
 // HDNode of BIP44 account
-let account = RVNBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'")
+let account = RVNBOX.HDNode.derivePath(masterHDNode, "m/0'/175'/0'")
 
 // derive the first external change address HDNode which is going to spend utxo
 let change = RVNBOX.HDNode.derivePath(account, '0/0')
 
-// get the rvn2 address
-let rvn2Address = RVNBOX.HDNode.toRvn2Address(change)
+// get the Legacy address
+let LegacyAddress = RVNBOX.HDNode.toLegacyAddress(change)
 
 let hex
 let txid
 
-RVNBOX.Address.utxo(rvn2Address).then(
+RVNBOX.Address.utxo(LegacyAddress).then(
   result => {
     if (!result[0]) {
       return
@@ -88,7 +88,7 @@ RVNBOX.Address.utxo(rvn2Address).then(
     let sendAmount = originalAmount - byteCount
 
     // add output w/ address and amount to send
-    transactionBuilder.addOutput(rvn2Address, sendAmount)
+    transactionBuilder.addOutput(LegacyAddress, sendAmount)
 
     // keypair
     let keyPair = RVNBOX.HDNode.toKeyPair(change)
@@ -108,7 +108,7 @@ RVNBOX.Address.utxo(rvn2Address).then(
     // output rawhex
     hex = tx.toHex()
 
-    // sendRawTransaction to running BCH node
+    // sendRawTransaction to running RVN node
     RVNBOX.RawTransactions.sendRawTransaction(hex).then(
       result => {
         txid = result
@@ -126,12 +126,12 @@ RVNBOX.Address.utxo(rvn2Address).then(
 
 let addresses = []
 for (let i = 0; i < 10; i++) {
-  let childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`)
-  addresses.push(RVNBOX.HDNode.toRvn2Address(childNode))
+  let childNode = masterHDNode.derivePath(`m/0'/175'/0'/0/${i}`)
+  addresses.push(RVNBOX.HDNode.toLegacyAddress(childNode))
 }
 
 export default {
-  name: 'BitBox',
+  name: 'RvnBox',
   data() {
     return {
       mnemonic: RVNBOX.Mnemonic.generate(256),
